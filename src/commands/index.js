754a9a78982bc7134aa3d5cae8f5db2d71600296
John <john.molinam@hotmail.com>
2016-10-07 18:02:51 -0400

    Move message delegation to Commands class

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,12 +1,43 @@
 import { readDir } from '../util/index';
+import env from '../environments';
+import _ from 'lodash';
 
 export default class {
-  static loadModules(bot) {
+  constructor(bot) {
+    this.bot = bot;
+
     let modules = readDir('./src/commands').map(file => {
       let klass = require(`./${file}`).default;
       return this[file] = new klass(bot);
     });
 
+    this.registerEvents;
     return Promise.all(modules);
+  }
+
+  get registerEvents() {
+    this.bot.on('message', message => this.handleMessage(message));
+  }
+
+  handleMessage(message) {
+    let { users } = message.mentions;
+    if (!_.isEmpty(this.isBot(users)) && !_.isEmpty(message.content)) {
+      let compose = this.compose(message.content);
+      this.delegate(compose, message);
+    }
+  }
+
+  isBot(users) {
+    return users.filter(u => u.username === env.BOT_NAME);
+  }
+
+  compose(message) {
+    let regex = new RegExp(/(\S+)\s{1}(\S+)\s{1}(\S+)\s{1}(.+)/);
+    let [,,id, cmd, msg] = message.match(regex);
+    return { id, cmd, msg };
+  }
+
+  delegate(compose, message) {
+    this[_.capitalize(compose.id)][`command:${compose.cmd}`](compose, message);
   }
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import Commands from './commands/index.js';
 
 Bot.login().then(bot => {
   console.log('Delegating bot to commands...');
-  Commands.loadModules(bot).then(promise => {
+
+  const C = new Commands(bot).then(promise => {
     console.log('Modules loaded.');
   });
 }).catch(console.log);
